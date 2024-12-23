3a8136ded3c4188b754aa5331e1d584eeb16b460
Efe Akaröz <69296379+efeakaroz13@users.noreply.github.com>
2024-12-23 10:41:05 +0300

    Update updater.py

--- updater.py
+++ updater.py
@@ -6,3 +6,5 @@ os.system("scp -r other efeakaroz13@45.155.124.85:/home/efeakaroz13/kentelPlatfo
 wannaContinue=input("wanna upload to the server_2 as well? (y/n):")
 if wannaContinue == "y":
     os.system("scp SERVER_2/*.py efeakaroz13@192.168.1.65:/home/efeakaroz13/kentelPlatform/SERVER_2/")
+
+#update
